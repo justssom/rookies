46a8d9cff78c3f47a4e91b27f4f9efa7f6398373
bellang2 <66351835+bellang2@users.noreply.github.com>
2025-02-27 13:36:54 +0900

    성환
    gitginore 변동사항 원위치

--- ROOTAIR_WEB-sub-main/blueprints/utils.py
+++ ROOTAIR_WEB-sub-main/blueprints/utils.py
@@ -5,11 +5,10 @@ def get_db_connection():
     return pymysql.connect(
         host='localhost',
         user='root',
-        password='1234',
-        database='mydatabase',
+        password='vndgh3538',
+        database='flask_root1234',
         charset='utf8mb4',
         use_unicode=True,
-        port=3307,  # ✅ 명확하게 포트 설정
         cursorclass=pymysql.cursors.DictCursor
     )
 
